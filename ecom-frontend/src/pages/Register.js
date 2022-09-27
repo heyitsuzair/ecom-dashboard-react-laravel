fde3c3ccb7ae3ae6bd6ad89a72705e156feaaca8
heyitsuzair <uzairdevil354123@gmail.com>
2022-09-27 20:44:50 +0500

    Register User Added

--- ecom-frontend/src/pages/Register.js
+++ ecom-frontend/src/pages/Register.js
@@ -1,5 +1,69 @@
-import React from "react";
-
+import React, { useState } from "react";
+import { Form, Button, Container } from "react-bootstrap";
+import axios from "axios";
+import { register } from "../utils/apis/apis";
+import { useNavigate } from "react-router-dom";
 export default function Register() {
-  return <div>Register</div>;
+  const navigate = useNavigate();
+
+  const [values, setValues] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+
+  const handleChange = (e) => {
+    setValues({ ...values, [e.target.name]: e.target.value });
+  };
+
+  const handleFormSubmit = async (e) => {
+    e.preventDefault();
+    await axios.post(register, values).then(({ data }) => {
+      localStorage.setItem("ecom-user", JSON.stringify(data));
+      navigate("/addProduct");
+    });
+  };
+
+  return (
+    <Container className="mt-4">
+      <Form onSubmit={(e) => handleFormSubmit(e)}>
+        <Form.Group className="mb-3" controlId="formBasicName">
+          <Form.Label>Name</Form.Label>
+          <Form.Control
+            onChange={(e) => handleChange(e)}
+            value={values.name}
+            name="name"
+            type="text"
+            placeholder="Enter Your Name"
+          />
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control
+            onChange={(e) => handleChange(e)}
+            value={values.email}
+            type="email"
+            name="email"
+            placeholder="Enter Email Address"
+          />
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            onChange={(e) => handleChange(e)}
+            value={values.password}
+            type="password"
+            name="password"
+            placeholder="Password"
+          />
+        </Form.Group>
+
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+      </Form>
+    </Container>
+  );
 }
